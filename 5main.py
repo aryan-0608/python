6ec96f9f64193f464dcbaf853175bf4fbf331c9d
Aryan Patel <ap9384907>
2025-11-27 22:01:03 +0530

    Escape , sequence & print statement

---
+++ 5main.py
@@ -0,0 +1,14 @@
+# Hey aryan, Please dont remove this line
+# Author: Aryan patel
+# Date: 2024-06-15
+
+print('Hey I am a \"good boy\"\nand this viewer is  also a good boy/girl')
+
+
+# print("Hello aryan")
+# #This is a 'single -line comment'
+# print("This is a print statement.")
+# print("Hello world !!!")  # printing hello world
+
+print("Hey", 6,7, sep="~", end="009\n")
+print("Aryan Patel")
