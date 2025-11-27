ce5c938de5b95053fd8b201a4d1adf1dc08bf927
Aryan Patel <ap9384907>
2025-11-27 21:20:38 +0530

    our First python program in basic

---
+++ 4main.py
@@ -0,0 +1,11 @@
+print("Hello aryan", 4)
+print(10)
+print("This is 4main.py")
+print(100*2)
+print("Addition is", 50+50)
+for i in range(3):
+    print("Line number", i+1)
+
+
+
+print("----Your poem here----")
