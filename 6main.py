438ef1a5394f9abb99d5a24940e2480749b0ecf5
Aryan Patel <ap9384907>
2025-11-28 16:25:35 +0530

    Varible and Data Types

---
+++ 6main.py
@@ -0,0 +1,24 @@
+# a = 2
+# print(a)
+a = complex(2,3)
+b = True
+c = "Aryan Patel"
+d = None
+print(a)
+print(b)
+a1 =9
+print(a + a1)
+print("the type of a is ",type(a))
+print("The type of b is ",type(b))
+print("The type of c is",type(c))
+
+list1 = [1,2.3,[-3,5],["apple","banana"]]
+print(list1)
+
+tuple1 = (1,2.3,(-3,5),("apple","banana"))
+print(tuple1)
+
+
+dict1 = {"name":"Aryan",
+         "age":22,}
+print(dict1)
