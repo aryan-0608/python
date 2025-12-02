03bb82852743030395906d92ed5be86edf2d3381
Aryan Patel <ap9384907>
2025-12-02 21:09:02 +0530

    String in python

---
+++ 11main.py
@@ -0,0 +1,17 @@
+name = "Aryan Patel"
+frind = "Aryan Veram"
+apple =''' hi aryan
+welcome to the world of python programming
+This is a multi-line string.
+'''
+print("hello, " + name)
+# print(apple)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+# print(name[5])  throw an error
+print("lets use  a for loop\n")
+for char in name:
+    print(char)
