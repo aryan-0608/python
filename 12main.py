27242bdf881f2f3a65bb2f0abf875b5821df47cd
Aryan Patel <ap9384907>
2025-12-03 13:39:54 +0530

    String Slicing and Operearions on string in python

---
+++ 12main.py
@@ -0,0 +1,17 @@
+fruit = "Mango"
+print(fruit)
+MangoLen = len(fruit)
+print(MangoLen)
+# print(fruit[0:4])  including 0 but not 4
+# print(fruit[1:4])   including 1 but not 4
+# print(fruit[:5])
+# print(fruit[0:-3])
+print(fruit[:len(fruit) -3])
+print(fruit[-4:-1])
+
+nm= "harry"
+print(nm[-4:-2])
+
+name = "Aryan Patel"
+for range in name:
+    print(len(name))
