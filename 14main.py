10ca355187e96b54075ba3bea05cfdec619031da
Aryan Patel <ap9384907>
2025-12-04 21:41:24 +0530

    basic, if , else

---
+++ 14main.py
@@ -0,0 +1,16 @@
+a = int(input("Enter your age :"))
+print("Your age is :", a)
+
+# conditional operators in Python
+# >,<,>=,<=,==,!=
+print(a > 18)
+print(a <= 18)
+print(a == 18)
+print(a != 18)
+if(a>18):
+    print("You are eligible for voting")
+    print("Have a nice day!")
+
+else:
+    print("You are not eligible for voting")
+    print("have the very nice day!")
