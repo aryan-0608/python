dc92328a07228fd9320d5fa820cd08029e04d558
Aryan Patel <ap9384907>
2025-12-04 21:47:40 +0530

    elif

---
+++ elif.py
@@ -0,0 +1,9 @@
+num = int(input("Enter the value if num:"))
+if (num < 0):
+    print("num is negative.")
+elif (num == 0):
+    print("Number is Zero.")
+else:
+    print("Number is postive.")
+
+    print("I am happy now")
