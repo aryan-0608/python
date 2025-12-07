e124b954044d7bb62e1fda457c9a70c140aac3f1
Aryan Patel <ap9384907>
2025-12-07 21:23:18 +0530

    For Loop in python

---
+++ 17main.py
@@ -0,0 +1,22 @@
+# name = 'Artificial Intelligence'
+# for i in name:
+#     print(i)
+#     if(i=="I"):
+#         print("This is important branch")
+
+
+# colors = ["Red","Green","Blue"]
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+# for a in range(5):
+#    print(a+1)
+
+# for k in range(1,9):
+#    print(k )
+
+for k  in range(1,10,2):
+   print(k )
+ 
