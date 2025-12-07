e3625f2142313fd5179849dbd6f1708ea439b96c
Aryan Patel <ap9384907>
2025-12-07 23:00:34 +0530

    While Loop

---
+++ 18main.py
@@ -0,0 +1,14 @@
+# i = int(input("Enter the number :"))
+# # i = 0
+# while(i<=38):
+#     i = int(input("Enter the number:"))
+#     print(i)
+#     # i = i + 1
+# print("Done")
+
+count = 5
+while(count > 0):
+    print(count)
+    count = count -1
+else:
+    print("Loop is ended")
