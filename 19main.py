b2568914ee145c4ce12cd0f77196e7850764e577
Aryan Patel <ap9384907>
2025-12-08 20:44:11 +0530

    break and continue in python

---
+++ 19main.py
@@ -0,0 +1,12 @@
+# for i in range(12):
+#     if(i == 8):
+#         print("skip the iteration")
+#         continue
+#     print("5 x", i, "=", 5*i)
+
+i = 0
+while True:
+        print(i)
+        i = i +1
+        if(i%30==0):
+            break
