87aa2d7e6e349498865e5f4a5335a808665e93cd
Aryan Patel <ap9384907>
2025-12-12 21:40:38 +0530

    Function in python

---
+++ 20main.py
@@ -0,0 +1,25 @@
+def calculateGmean(a,b):
+   mean = (a*b)/(a+b)
+   print(mean)
+
+a = 9
+b = 8
+if(a>b):
+        print("first number is greater")
+else:
+        print("second number is greater or equal")
+
+# gmean = (a*b)/(a+b)
+# print(gmean)  
+        
+calculateGmean(a, b)
+c =8
+d = 7
+if(c>d):
+    print("first number is greater")
+else:
+        print("second number is greater or equal")
+
+# gmean2 = (c*d)/(c+d)
+# print(gmean2)
+calculateGmean(c,d)
