ebf2c3320d82edc043e737e35abdb2b9172ab0d7
Aryan Patel <ap9384907>
2025-12-13 14:56:16 +0530

    List Method in python

---
+++ 23main.py
@@ -0,0 +1,15 @@
+list = [11,45,1,2,34,5, 5]
+print(list)
+# list.append(99)
+# list.sort(reverse=True)
+# list.reverse()
+# print(list.index(1))
+# print(list.count(5))
+# m = list.copy()
+# list.insert(2, 67)
+m= [900,100,300]
+k= list+m
+print(k)
+list.extend(m)
+
+print(list)
