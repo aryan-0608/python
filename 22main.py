c699a6cad0f3c1477e2b1d375771e7b68cfc97cf
Aryan Patel <ap9384907>
2025-12-13 14:35:30 +0530

    Introduction to list

---
+++ 22main.py
@@ -0,0 +1,35 @@
+list = [3,4,5, "Aryan",True]
+# print(list)
+# print(type(list))
+# print(list[0])
+# print(list[1])
+# print(list[2])
+# print(list[3])
+# print(list[4])
+# print(list[5])
+
+# print(list[-3]) # Negative index
+# print(list[len(list)-3]) # positive index
+
+# print(list[5-3]) # positive index
+# print(list[2]) # positive index
+
+# if "Aryan" in list:
+#     print("Yes Aryan is present in the list")
+# else:
+#     print("No Aryan is not present in the list")
+
+ #same thing applies for string as well
+# if "yan" in "Aryan":
+ #print("yes")
+
+# print(list)
+# print(list[1:-1])
+# print(list[1:4:2])
+
+list =[i*i for i in range(10)]
+print(list)
+
+list =[i*i for i in
+ range(10) if i%2==0]
+print(list)
