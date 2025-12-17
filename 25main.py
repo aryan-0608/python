0924a000ec660768a7814edf97eea136e60b4f22
Aryan Patel <ap9384907>
2025-12-17 16:50:39 +0530

    opertor in tuple

---
+++ 25main.py
@@ -0,0 +1,23 @@
+# countries =("USA", "Canada", "Germany", "France", "Australia")
+# temp = list(countries)
+# temp.append("India")
+# temp.pop(2)
+# temp[2]= "Italy"
+# countries = tuple(temp)
+# print(countries)
+
+# countries =("USA", "Canada", "Germany", "France", "Australia")
+# countries2 =("India", "China")
+# south_asia = countries + countries2
+# print(south_asia)
+
+
+tuple1 =(0,1,2,3,5,7,8, 9)
+print(tuple1)
+# res =tuple1.count(3)
+# res =tuple1.index(5)
+# res = tuple1.index(344)
+# res = tuple1.index(3,4,8)
+res = len(tuple1)
+
+print('count of 3 in tuple1 is:',res)
