c599e5c5c6525a684e0f39010591a1f5e7363642
Aryan Patel <ap9384907>
2025-12-22 20:28:33 +0530

    Set of python

---
+++ 31main.py
@@ -0,0 +1,13 @@
+s = {2,3,4,3,5}
+print(s)
+print(type(s))
+
+info = {"aryan",19,False,3.8,19}
+print(type(info))
+
+Aryan = set(("Aryan","Rohan","Sonam"))
+print(Aryan)
+print(type(Aryan))
+
+for value in info:
+    print(value)
