7fd2fb99ab411224d518ae860eb6c2425412f979
Aryan Patel <ap9384907>
2025-12-27 21:35:46 +0530

    Dictionary Method

---
+++ 34main.py
@@ -0,0 +1,11 @@
+D1 = {122: 45, 124: 67, 126: 89}
+D2 = {125: 90, 127: 12, 128: 34}
+# D1.update(D2)
+# print(D1, D2)
+# print(D1)
+# D1.clear()
+# D1.pop(122)
+# D1.popitem()
+del D1[124]
+
+print(D1)
