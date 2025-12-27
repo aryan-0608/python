6fefc13ef83739db086116ce76aeec9968387cdd
Aryan Patel <ap9384907>
2025-12-27 21:53:15 +0530

    For loop with else in python use

---
+++ 35main.py
@@ -0,0 +1,24 @@
+# for i  in range(5):
+#     print(i)
+#     print("Hello, World!")
+#     if i==4:
+#       break
+# else:
+#     print("sorry no i")
+
+# i = 0
+# while i<7:
+#     print(i)
+#     i = i + 1
+#     if i ==5:
+#         break
+# else:
+#     print("sorry no i")
+
+for x in range(5):
+    print("iteration no {} in for loop".format(x+1))
+
+else:
+    print("else blocl in loop"
+          )
+    print("else loop")
