3a0bb77c08c54e86fd8410c8eaab171cef015459
Aryan Patel <ap9384907>
2026-01-09 22:18:08 +0530

    Enumerate Function

---
+++ 42main.py
@@ -0,0 +1,24 @@
+marks = [12,56,32,98,45,67,89,23,45,67]
+
+# index = 0
+# for mark in marks:
+#     print(mark)
+#     if(index == 3):
+#      print("Aryan  awesome!")
+#     index += 1
+
+# 
+
+# for index, mark in enumerate(marks):
+#     print(mark)
+#     if(index == 3):
+#      print("Aryan  awesome!")
+
+# 
+# colours = ["red","green","blue","yellow"]
+# for index, colour in enumerate(colours):
+#     print(index,colour)
+
+s = "hello"
+for index,c in enumerate(s):
+        print(index,c)
