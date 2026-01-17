0093b1829191e265aa2e24fa5da0d111c1a21018
Aryan Patel <ap9384907>
2026-01-17 14:31:27 +0530

    How import work in python

---
+++ 44main.py
@@ -0,0 +1,19 @@
+# from math import sprt,pi
+# from math import pi,sprt as s
+# import math as math_builtin_python
+
+
+# result = math_builtin_python.sqrt(9)
+# math_builtin_python.pi
+# print(result)
+
+# from aryan import welcome,aryan
+# from aryan import*
+import aryan as ar
+import math
+
+from aryan import welcome
+print(dir(math))
+print(math.nan,type(math.nan))
+ar.welcome()
+print(ar.aryan)
