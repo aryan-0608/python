e5e1ac9d971d4daaf5874f4a5a128311001c5cbc
Aryan Patel <ap9384907>
2026-03-15 14:27:39 +0530

    Maagic/Dunder methods

---
+++ 73main.py
@@ -0,0 +1,16 @@
+# class Employee:
+#   def __init__(self, name):
+#     self.name = name
+
+#     # name = "Aryan"
+#   def __len__(self):
+#     i = 0
+#     for c in self.name:
+#       i = i + 1
+#     return i
+from emp import Employee
+
+e = Employee("Aryan")
+print(e)
+# print(e.name)
+# print(len(e))
