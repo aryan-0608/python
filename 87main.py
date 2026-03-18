d108a7f363560ae8f589691a48fd5d37801d8807
Aryan Patel <ap9384907>
2026-03-18 15:38:09 +0530

    shutil Module in python

---
+++ 87main.py
@@ -0,0 +1,8 @@
+import shutil
+
+# shutil.copy("main.py", "main2.copy.py")
+
+# shutil.copytree("72main.py", "./mytutorial")
+
+# shutil.move("main2.copy.py", "main3.copy.py")
+shutil.rmtree("./mytutorial")
